3bfb484930a671e292e014f10c15186164ca5999
Bryan Calisto <bryancalisto@yahoo.es>
2021-11-14 09:21:24 -0500

    New: Added function to create a dependency graph

--- simple_js_bundler/bundler.js
+++ simple_js_bundler/bundler.js
@@ -1,11 +1,54 @@
 import fs from 'fs';
 import babylon from 'babylon';
+import babelTraverse from 'babel-traverse';
+import path from 'path';
+
+let fileId = 0;
 
 function createAsset(filename) {
+  // Read the source code
   const src = fs.readFileSync(filename, 'utf-8');
+  // Parse the source to a AST (abstract syntax tree) so later we can find 'import' tokens easily
   const parsedAst = babylon.parse(src, { sourceType: 'module' });
-  console.log(parsedAst);
+  // Extract 'ImportDeclaration' nodes (the child dependencies)
+  const dependencies = [];
+  babelTraverse.default(parsedAst, {
+    ImportDeclaration: ({ node }) => {
+      dependencies.push(node.source.value);
+    }
+  })
+
+  return {
+    id: fileId++,
+    filename,
+    dependencies
+  }
 }
 
-const rootFolder = 'src';
-createAsset(rootFolder + '/author.js');
+function createGraph(entryFile) {
+  const directory = path.dirname(entryFile);
+
+  // Read the dependencies of the entry file
+  const entryAsset = createAsset(entryFile);
+
+  // In this list we will have our dependencies graph
+  const assets = [entryAsset];
+
+  // Going to use iterative approach, not recursive
+  for (const asset of assets) {
+    //The associations between this asset and others will be here
+    asset.mapping = {};
+
+    // Get the child assets from each dependency
+    asset.dependencies.forEach(relativePath => {
+      const childAsset = createAsset(path.join(directory, relativePath));
+      asset.mapping[relativePath] = childAsset.id;
+      assets.push(childAsset);
+    });
+  }
+
+  return assets;
+}
+
+const graph = createGraph('src\\entry.js');
+console.log(graph);
